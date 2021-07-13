b3d53e551498beb3b8c6e628e7362b34b092d743
akashjha011 <akash.jsmith@gmail.com>
2021-07-13 12:03:36 +0530

    Update GeoTokens.sol
    Added bidding logic and resale logic to the contract

--- GeoTokens.sol
+++ GeoTokens.sol
@@ -22,18 +22,29 @@ contract GeoTokens is ERC721,Ownable {
     }
     
     struct resaleInfo{
-        bool reserved;
+        uint256 highestBid;
+        address bidderAddress;
         uint256 resalePrice;
         uint256 tokenID;
+        uint256 resaleTime;
     } 
     
-    resaleInfo[] private ResaleTokens;
+    struct bidInfo{
+        uint256 highestBid;
+        address bidderAddress;
+    }
     
+    
+    resaleInfo[] public ResaleTokens;
+    mapping(uint256=>uint256) public TokenSaleTime;
+    mapping(uint256=>bidInfo) AuctionInfo;
     mapping(uint16 => bool) layerLocked;
-    mapping(address=>bool) approvedUsers;
+    mapping(address=>bool) public approvedUsers;
+    mapping(address=>uint256) public salesBalance;
+    
     event layerLock(uint16 layerNumber,bool layerStatus);
-    event NFTCreation(uint256 tokenID,tokenInfo Info);
-    event NFTSale(address buyer,uint256 tokenId);
+    event NFTCreation(uint256 tokenID,tokenInfo Info,uint256 saleTime);
+    event NFTBid(bidInfo newBid,uint256 tokenId);
     
     constructor() ERC721("GeoTokens","GT"){
         tokenId = 1;
@@ -44,9 +55,6 @@ contract GeoTokens is ERC721,Ownable {
         approvedUsers[UserAddress] = true;
     }
     
-    function isApprovedUser(address UserAddress) internal view returns(bool){
-        return approvedUsers[UserAddress];
-    }
     
     modifier onlyApprovedOrOwner(address UserAddress){
         require(UserAddress == owner() || approvedUsers[UserAddress],"GeoTokens : User is not owner or Approved");
@@ -59,21 +67,32 @@ contract GeoTokens is ERC721,Ownable {
         metaData[tokenID].price = _price;
     }
     
+    //Change locked status of layer
     function changeLayerLock(uint16 layerNumber,bool status) external onlyApprovedOrOwner(msg.sender){
         require(layerLocked[layerNumber] != status,"GeoTokens : Locked status doesn't change");
         layerLocked[layerNumber] = status;
         emit layerLock(layerNumber,status);
     }
     
-    //Owner can mint a new NFT 
-    function CreateNew(tokenInfo[] memory MetaData,string[] memory svg) external onlyApprovedOrOwner(msg.sender) {
+    //Change end date of auction
+    function changeSaleTime(uint256 tokenID,uint256 newDaysLater) external onlyApprovedOrOwner(msg.sender){
+        require(metaData[tokenID].status == 1,"GeoTokens : Can't change auction end date for sold tokens");
+        require(tokenID < tokenId,"GeoTokens : NFT doesn't exist");
+        TokenSaleTime[tokenId] = block.timestamp + newDaysLater * 1 days;
+    }
+    
+    //Owner can mint a new NFT and host for auction
+    function CreateNew(tokenInfo[] memory MetaData,string[] memory svg,uint256[] memory DaysLater) external onlyApprovedOrOwner(msg.sender) {
         require(MetaData.length == svg.length,"GeoTokens: MetaData and Svg lenghts don't match");
+        require(MetaData.length == DaysLater.length,"GeoTokens: MetaData and aunction days length don't match");
         uint256 length = MetaData.length;
         uint256 j;
+        //store metadata, svg and unix sale datelimit 
         for(j=0;j<length;j++){
-            metaData[tokenId] = MetaData[j];
-            tokenSvg[tokenId] = svg[j];
-            emit NFTCreation(tokenId,metaData[tokenId]);
+            metaData[tokenId+j] = MetaData[j];
+            tokenSvg[tokenId+j] = svg[j];
+            TokenSaleTime[tokenId+j] = block.timestamp + DaysLater[j] * 1 days;
+            emit NFTCreation(tokenId,metaData[tokenId],TokenSaleTime[tokenId]);
             tokenId = tokenId + 1;
         }
     }
@@ -84,22 +103,41 @@ contract GeoTokens is ERC721,Ownable {
     }
     
     //Owner can retrieve ONE stored in contract
-    function retrieve(address payable transferAddress) external onlyApprovedOrOwner(msg.sender){
-        require(address(this).balance > 0,"GeoTokens: No Ether to retrieve");
-        payable(transferAddress).transfer(address(this).balance);
+    function retrieve() external {
+        require(salesBalance[msg.sender] > 0,"GeoTokens: No Ether to retrieve");
+        payable(msg.sender).transfer(salesBalance[msg.sender]);
     }
     
     
-    //Users can pay prescribed amount and buy token of particular token ID
-    function Buy(uint256 tokenID) external payable{
+    function Bid(uint256 tokenID) external payable{
         require (tokenID < tokenId,"GeoTokens: Token doesn't exist");
         require (metaData[tokenID].status == 1,"GoeTokens: Token is already sold");
         require (!layerLocked[metaData[tokenID].layer],"GeoTokens: This layer is locked right now");
-        require (msg.value >= metaData[tokenID].price, "GeoTokens: Pay equal to or more than set Price");
-        metaData[tokenID].status = 0;//Token status is sold
-        _safeMint(msg.sender, tokenID);
-        emit NFTSale(msg.sender,tokenID);
+        require ((msg.value >= metaData[tokenID].price) && (msg.value >= AuctionInfo[tokenID].highestBid + 0.1 ether), "GeoTokens: Pay more than highest bidder");
+        require(block.timestamp < TokenSaleTime[tokenID], "GeoTokens: Auction has already expired");
+        bidInfo memory newBid;
+        if(AuctionInfo[tokenID].bidderAddress != address(0)){
+            newBid.highestBid = AuctionInfo[tokenID].highestBid;
+            newBid.bidderAddress = AuctionInfo[tokenID].bidderAddress;
+            AuctionInfo[tokenID].highestBid = 0;
+            AuctionInfo[tokenID].bidderAddress = address(0);
+            payable(newBid.bidderAddress).transfer(newBid.highestBid);
+        }
+        newBid.highestBid = msg.value;
+        newBid.bidderAddress = msg.sender;
+        AuctionInfo[tokenID] = newBid;
+        emit NFTBid(AuctionInfo[tokenID],tokenID);
         
+    }
+    
+    //Users can retrieve tokens won in the auction
+    function RetrieveNFT(uint256 tokenID) external payable{
+        require(AuctionInfo[tokenID].bidderAddress == msg.sender,"GeoTokens: User is not auction winner for this token");
+        require(metaData[tokenID].status == 1,"GeoTokens: NFT has alread been sold");
+        metaData[tokenID].status = 0;//Token status is sold
+        salesBalance[owner()] += AuctionInfo[tokenID].highestBid;
+        delete TokenSaleTime[tokenID];
+        _safeMint(msg.sender, tokenID);
     }
     
     //returns metadata based on token ID
@@ -160,31 +198,32 @@ contract GeoTokens is ERC721,Ownable {
         return metaInfo;
     }
     
+    //make pure frontend ?
     function enableResale() external {
         setApprovalForAll(address(this),true);
     }
     
+    //Make pure frontend??
     function disableResale() external {
         setApprovalForAll(address(this),false);
     }
     
-    function putTokenForResale(bool isReserved,uint256 price,uint256 TokenID) external {
-        require(ownerOf(TokenID) == msg.sender,"GeoTokens: User is not the owner of NFT");
+    function putTokenForResale(uint256 price,uint256 TokenID,uint256 daysAfter) external {
+        require(ownerOf(TokenID) == msg.sender,"GeoTokens: User is not the owner of this NFT");
         resaleInfo memory newInfo;
-        newInfo.reserved = isReserved;
         newInfo.resalePrice = price;
         newInfo.tokenID = TokenID;
+        newInfo.resaleTime = block.timestamp + daysAfter * 1 days;
         ResaleTokens.push(newInfo);
     }
     
-    function reSale(uint256 resaleID,uint256 TokenID) public payable{
+    function RetrieveReSale(uint256 resaleID,uint256 TokenID) public payable{
         require(ResaleTokens[resaleID].tokenID == TokenID, "GeoTokens: Token ID mismatch");
-        require(msg.value == ResaleTokens[resaleID].resalePrice,"Price is not equal to seller defined price");
-        transferFrom(ownerOf(TokenID),msg.sender,TokenID);
-    }
-    
-    function contractBalance() external view returns(uint256){
-        return address(this).balance;
+        require(block.timestamp > ResaleTokens[resaleID].resaleTime, "GeoTokens: Auction has not ended yet");
+        require(ResaleTokens[TokenID].bidderAddress == msg.sender,"GeoTokens: User is not the highest bidder");
+        salesBalance[ownerOf(TokenID)] += ResaleTokens[resaleID].highestBid;
+        safeTransferFrom(ownerOf(TokenID),msg.sender,TokenID);
+        delete ResaleTokens[resaleID];
     }
     
 }
